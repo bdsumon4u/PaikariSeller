50a8a53a1de8943d5293ea1f07a8bb1893bf1279
Sumon Ahmed <bdsumon4u@gmail.com>
2023-01-12 12:04:36 +0600

    BugFix: ElitBuzz SMS Gateway

--- app/Utility/SendSMSUtility.php
+++ app/Utility/SendSMSUtility.php
@@ -199,7 +199,7 @@ class SendSMSUtility
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             $response = curl_exec($ch);
